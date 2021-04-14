28adaf8f949b299edbbd644e97996cc0c617877d
apzr <iswallow1l2l@gmail.com>
2021-04-14 09:00:20 +0800

    add self4j

--- src/main/java/com/yuepong/jdev/pulsar/kit/ArgsResolver.java
+++ src/main/java/com/yuepong/jdev/pulsar/kit/ArgsResolver.java
@@ -1,20 +1,25 @@
 package com.yuepong.jdev.pulsar.kit;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @ClassName: ArgsResolver
  * @Description:
  * @Author: apr
  * @Date: 2021/04/09 11:40:53
  **/
+@Slf4j
 public class ArgsResolver {
     public static String getParam(String args[], String argName, Object defaultValue){
+        String param = defaultValue.toString();
         String target = "--".concat(argName).concat("=");
         if (args != null) for (String p : args) {
             if (p.startsWith(target)){
-                 return p.substring( 3+argName.length() );
+                param =  p.substring( 3+argName.length() ) ;
             }
         }
 
-        return defaultValue.toString();
+        log.info("Param\t"+argName+"\t\t{}", param );;
+        return param;
     }
 }
--- src/main/java/com/yuepong/jdev/pulsar/transfer/PulsarConsumers.java
+++ src/main/java/com/yuepong/jdev/pulsar/transfer/PulsarConsumers.java
@@ -1,5 +1,6 @@
 package com.yuepong.jdev.pulsar.transfer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.client.api.*;
 import org.apache.pulsar.client.impl.MessageIdImpl;
 import com.yuepong.jdev.pulsar.common.Config;
@@ -12,6 +13,7 @@ import com.yuepong.jdev.pulsar.kit.ArgsResolver;
  * @Author: apr
  * @Date: 2021/04/09 08:44:54
  **/
+@Slf4j
 public class PulsarConsumers {
 
 	private static PulsarClient client;
@@ -24,16 +26,6 @@ public class PulsarConsumers {
 		long ledgerId = Long.parseLong( ArgsResolver.getParam(args, "lid", Config.LEDGER_ID) );
 		long entryId = Long.parseLong( ArgsResolver.getParam(args, "eid", Config.ENTRY_ID) );
 		int partitionIndex = Integer.parseInt( ArgsResolver.getParam(args, "pid", Config.PARTITION_INDEX) );
-
-		System.out.printf("\r\n");
-		System.out.printf("**************************************************\r\n");
-		System.out.printf("Param\turl\tSERVICE_URL\t\t%s", serviceUrl + "\r\n");
-		System.out.printf("Param\tlid\tLEDGER_ID\t\t%s", ledgerId + "\r\n");
-		System.out.printf("Param\teid\tENTRY_ID\t\t%s", entryId + "\r\n");
-		System.out.printf("Param\tpid\tPARTITION_INDEX\t\t%s", partitionIndex + "\r\n");
-		System.out.printf("Param\tfrom\tCONSUME\t\t%s", from + "\r\n");
-		System.out.printf("Param\tto\tPRODUCE\t\t%s", to + "\r\n");
-		System.out.printf("**************************************************\r\n");
 
 		client = PulsarClient.builder().serviceUrl(serviceUrl).build();
 		MessageId id = new MessageIdImpl(ledgerId, entryId, partitionIndex);
@@ -49,8 +41,8 @@ public class PulsarConsumers {
 			Producer<byte[]> producer = client.newProducer().topic(to).create();
 			producer.send(msg.getData());
 
-			System.out.printf("Transfering: %s\tCount: %s\tmessage:\r\n", msg.getMessageId(), count++);
-			System.out.printf(new String(msg.getData(),"UTF-8") + "\r\n");
+			log.info("Transferring: {}\tCount: {}", msg.getMessageId(), count++);
+			log.debug("Message:\t{}", new String(msg.getData(),"UTF-8"));
 		}
 	}
 }
